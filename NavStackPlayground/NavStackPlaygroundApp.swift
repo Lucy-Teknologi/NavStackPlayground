69f625584a7511746566c1195776c8b17b9a542e
Gilbert Thiodores <gthiodores71@gmail.com>
2023-09-22 16:13:09 +0800

    Initial Commit

---
+++ NavStackPlayground/NavStackPlaygroundApp.swift
@@ -0,0 +1,17 @@
+//
+//  NavStackPlaygroundApp.swift
+//  NavStackPlayground
+//
+//  Created by Lucy on 22/09/23.
+//
+
+import SwiftUI
+
+@main
+struct NavStackPlaygroundApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
